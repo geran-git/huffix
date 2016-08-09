c78dfead095bde3a39fc3fcddaed754a78fa3bc5
geran-git <mycom@tut.by>
2016-08-09 23:06:56 +0300

    Just general thoughts about compressing algorithm

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+/*
+
+*/
 #include <stdio.h>
 #include <strings.h>
 int main(int argc, char *argv[])
@@ -19,8 +22,17 @@ int main(int argc, char *argv[])
   	/*Check mode option*/
   	if ( 0 == strcmp(argv[2], "-c"))
   	{
+      int bitcode[256]; = {0};
+
   		/*Going to compress*/
   		printf ("Compressing, please wait...\n");
+      /* Let's build code table later */
+      /* The main goal for this moment is to implement code lookup and transform bit sequence to bytes */
+      /* Lookup table is simple - array where code_value = bitcode[byte_to_encode] */
+      /* Let's fill table with codes later */
+
+
+
   	}
   	else if ( 0 == strcmp(argv[2], "-x"))
   	{
