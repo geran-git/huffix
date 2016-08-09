c506e1e58cca706f3b8165251a2c8dba0d700ed5
geran-git <mycom@tut.by>
2016-08-09 09:49:13 +0300

    Command line parameters  check

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
-void main(void)
+#include <strings.h>
+int main(int argc, char *argv[])
 {
-    printf("Hello Huffix!");
-    return 0;
-}
+  int count;
+
+  printf ("\nHuffiX archiver - Version 1.0 - 2016\n");
+
+  if (4 != argc)
+  {
+  	/*Quick help*/
+  	printf("Usage: huff ifile [-c|-x] ofile\n");
+  	printf("\t-c\t compress ifile to ofile\n");
+  	printf("\t-x\t extract ifile to ofile\n");
+  	return 0;
+  }
+  else
+  {
+  	/*Check mode option*/
+  	if ( 0 == strcmp(argv[2], "-c"))
+  	{
+  		/*Going to compress*/
+  		printf ("Compressing, please wait...\n");
+  	}
+  	else if ( 0 == strcmp(argv[2], "-x"))
+  	{
+  		/*Going to extract*/
+  		printf ("Extracting, please wait...\n");
+  	}
+  	else
+  	{
+ 		printf ("Wrong argument (%s) specified\n", argv[2]);
+   	}
+  }
+  /*
+    for (count = 1; count < argc; count++)
+	{
+	  printf("argv[%d] = %s\n", count, argv[count]);
+	}
+    */
+   return 0;
+ }
 
