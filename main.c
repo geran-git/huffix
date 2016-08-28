7abc7d55febb5a90a79de17546d42fc48291385e
geran-git <mycom@tut.by>
2016-08-28 22:52:01 +0300

    Compress mode implemented

--- main.c
+++ main.c
@@ -7,8 +7,8 @@
 #include <string.h>
 #include <time.h>
 
-#define RD_BUF_SIZE 256
-#define WR_BUF_SIZE 256
+#define RD_BUF_SIZE (256*1024)
+#define WR_BUF_SIZE (256*1024)
 /*Because bits of huffman code represented as 0 and 1 bytes*/
 /*buffer for encoded data must be 8 times bigger than write buffer*/
 #define HF_BUF_SIZE (WR_BUF_SIZE*8) 
@@ -38,11 +38,11 @@ uchar code_tbl[256][256] = {0};
 uchar max_len = 0;
 
 /*buffer for byte sequence representation of encoded data*/
-uchar *hf_buf = NULL;
+//uchar *hf_buf = NULL;
+uchar hf_buf[HF_BUF_SIZE];
 
 int main(int argc, char *argv[])
 {
-  int count;
   time_t time1, time2;
   char* c_time_string;
 
@@ -71,7 +71,7 @@ int main(int argc, char *argv[])
       long fsize = 0L;
       long l = 0l;
       int bytes_read = 0;
-      int i = 0;
+      int iCnt = 0;
 
       FILE *f_in = fopen(argv[1], "r");
 
@@ -93,9 +93,9 @@ int main(int argc, char *argv[])
             bytes_read = fread(rd_buf, 1, RD_BUF_SIZE, f_in);
             fsize += bytes_read;
 
-            for (i = 0; i < bytes_read; i++)
+            for (iCnt = 0; iCnt < bytes_read; iCnt++)
             {
-              freq_tbl[rd_buf[i]] += 1;
+              freq_tbl[rd_buf[iCnt]] += 1;
             }
           }
           while (0 == feof(f_in));
@@ -103,10 +103,10 @@ int main(int argc, char *argv[])
           printf("Bytes processed: %ld\n", fsize);
           printf("Frequency table:\n");
           
-          for (i = 0; i < 256; i++)
+          for (iCnt = 0; iCnt < 256; iCnt++)
           {
-            printf("%.2X: %ld ", i, freq_tbl[i]);
-            if ((i+1)%8 == 0) printf("\n");
+            printf("%.2X: %ld ", iCnt, freq_tbl[iCnt]);
+            if ((iCnt+1)%8 == 0) printf("\n");
           }
           /*Scaling frequencies*/
           long scale = fsize / (long)(UINT_MAX)+256L;
@@ -116,12 +116,12 @@ int main(int argc, char *argv[])
           /*Fill tree leaves*/
           int tree_cnt = 0;
 
-          for (i = 0; i < 256; i++)
+          for (iCnt = 0; iCnt < 256; iCnt++)
           {
-            if (freq_tbl[i] > 0) 
+            if (freq_tbl[iCnt] > 0) 
             {
-              tree[tree_cnt].code = i;
-              tree[tree_cnt].freq = (uint)(freq_tbl[i]/scale)+1u;
+              tree[tree_cnt].code = iCnt;
+              tree[tree_cnt].freq = (uint)(freq_tbl[iCnt]/scale)+1u;
               tree_cnt++;
 
               printf("%.2X: %d ", tree[tree_cnt-1].code, tree[tree_cnt-1].freq);
@@ -132,59 +132,59 @@ int main(int argc, char *argv[])
           printf("\n");
 
           /*Building tree*/
-          int i1, i2;
-          int freq1, freq2;
+          int iNode1, iNode2;
+          int iFreq1, iFreq2;
 
           do 
           {
             /*1. Determine two leaves with smallest frequencies*/
-            i1 = -1;
-            i2 = -1;
-            freq1 = 0;
-            freq2 = 0;
-            /*i1 - index of node with smallest frequency (for current step)*/
-            /*i2 - index of second node (updated with i1 when i1 changes)*/
+            iNode1 = -1;
+            iNode2 = -1;
+            iFreq1 = 0;
+            iFreq2 = 0;
+            /*iNode1 - index of node with smallest frequency (for current step)*/
+            /*iNode2 - index of second node (updated with iNode1 when iNode1 changes)*/
 
-            for (i = 0; i < tree_cnt; i++)
+            for (iCnt = 0; iCnt < tree_cnt; iCnt++)
             {
-              if (0 == tree[i].up)
+              if (0 == tree[iCnt].up)
               {
                 /*Proceed searching nodes with smallest frequencies*/
-                if ((0 <= i1) && (0 <= i2))
+                if ((0 <= iNode1) && (0 <= iNode2))
                 {
-                  if (freq1 >= tree[i].freq)
+                  if (iFreq1 >= tree[iCnt].freq)
                   {
-                    freq2 = freq1;
-                    i2 = i1;
-                    i1 = i;
-                    freq1 = tree[i].freq;
+                    iFreq2 = iFreq1;
+                    iNode2 = iNode1;
+                    iNode1 = iCnt;
+                    iFreq1 = tree[iCnt].freq;
                   }
                 }
-                else if ((-1 == i1) && (-1 == i2))
+                else if ((-1 == iNode1) && (-1 == iNode2))
                 {
                   /*Initial value - First node*/
-                  if (tree[i].freq > 0)
+                  if (tree[iCnt].freq > 0)
                   {
-                    i1 = i;
-                    freq1 = tree[i].freq;
+                    iNode1 = iCnt;
+                    iFreq1 = tree[iCnt].freq;
                   }
                 }
-                else if ((-1 == i2) && (0 <= i1))
+                else if ((-1 == iNode2) && (0 <= iNode1))
                 {
-                  if (tree[i].freq > 0)
+                  if (tree[iCnt].freq > 0)
                   {
                     /*Initial value - Second node*/
-                    if (tree[i].freq >= freq1)
+                    if (tree[iCnt].freq >= iFreq1)
                     {
-                      i2 = i;
-                      freq2 = tree[i].freq;
+                      iNode2 = iCnt;
+                      iFreq2 = tree[iCnt].freq;
                     }
                     else
                     {
-                      i2 = i1;
-                      freq2 = freq1;
-                      i1 = i;
-                      freq1 = tree[i].freq;
+                      iNode2 = iNode1;
+                      iFreq2 = iFreq1;
+                      iNode1 = iCnt;
+                      iFreq1 = tree[iCnt].freq;
                     }
                   }
                 }
@@ -195,52 +195,52 @@ int main(int argc, char *argv[])
 
             /*2. If two elements found create new node*/
             /*  and add it to the end of array*/
-            if (0 <= i1 && 0 <= i2  )
+            if (0 <= iNode1 && 0 <= iNode2  )
             {
-              tree[tree_cnt].freq = freq1 + freq2;
-              tree[tree_cnt].left = i1;
-              tree[tree_cnt].right = i2;
-              tree[i1].up = tree_cnt;
-              tree[i2].up = tree_cnt;
+              tree[tree_cnt].freq = iFreq1 + iFreq2;
+              tree[tree_cnt].left = iNode1;
+              tree[tree_cnt].right = iNode2;
+              tree[iNode1].up = tree_cnt;
+              tree[iNode2].up = tree_cnt;
 
-              printf("Left Node \t[%d] Freq %d Left %d Right %d Up %d\n", i1, tree[i1].freq, tree[i1].left, tree[i1].right, tree[i1].up);         
-              printf("Right Node \t[%d] Freq %d Left %d Right %d Up %d\n", i2, tree[i2].freq, tree[i2].left, tree[i2].right, tree[i2].up);         
+              printf("Left Node \t[%d] Freq %d Left %d Right %d Up %d\n", iNode1, tree[iNode1].freq, tree[iNode1].left, tree[iNode1].right, tree[iNode1].up);         
+              printf("Right Node \t[%d] Freq %d Left %d Right %d Up %d\n", iNode2, tree[iNode2].freq, tree[iNode2].left, tree[iNode2].right, tree[iNode2].up);         
               printf("Up node \t[%d] Freq %d Left %d Right %d Up %d\n", tree_cnt, tree[tree_cnt].freq, tree[tree_cnt].left, tree[tree_cnt].right, tree[tree_cnt].up);
               tree_cnt++;
             }
 
             /*Only one free node found - tree completed */
-          } while (-1 != i2);
+          } while (-1 != iNode2);
 
           printf("\nTree completed, total nodes %d...\n", tree_cnt);
 
           
 
           /*Generate code table*/
-          for (i = 0; i < tree_cnt; i++)
+          for (iCnt = 0; iCnt < tree_cnt; iCnt++)
           {
             /*For each leave build code sequence*/
-            if (0 == tree[i].left)
+            if (0 == tree[iCnt].left)
             {
               uchar len = 0;
-              uint node = i;
+              uint node = iCnt;
 
               printf("Code \t[%d] Sequence ", tree[node].code);
 
               while(0 != tree[node].up)
               {
-                code_tbl[tree[i].code][len+1] = (node == tree[tree[node].up].left) ? 0 : 1;
+                code_tbl[tree[iCnt].code][len+1] = (node == tree[tree[node].up].left) ? 0 : 1;
                 node = tree[node].up;
                 len++;
-                printf("%d", code_tbl[tree[i].code][len]);
+                printf("%d", code_tbl[tree[iCnt].code][len]);
                /* if (255 == len) break;*/
               }
 
-              code_tbl[tree[i].code][0] = len;
+              code_tbl[tree[iCnt].code][0] = len;
 
               if (max_len < len) max_len = len;
 
-              printf(" %d\n", code_tbl[tree[i].code][0]);
+              printf(" %d\n", code_tbl[tree[iCnt].code][0]);
               /*Bytes now stored in MSB->LSB order, but it would be more convinient*/
               /*to build bit stream if we put them in LSB->MSB order */  
               if (len >= 2)
@@ -249,39 +249,69 @@ int main(int argc, char *argv[])
                 uchar temp = 0;
                 for (j = 0; j < len/2; j++)
                 {
-                  temp = code_tbl[tree[i].code][j+1];
-                  code_tbl[tree[i].code][j+1] = code_tbl[tree[i].code][len - j];
-                  code_tbl[tree[i].code][len - j] = temp;
+                  temp = code_tbl[tree[iCnt].code][j+1];
+                  code_tbl[tree[iCnt].code][j+1] = code_tbl[tree[iCnt].code][len - j];
+                  code_tbl[tree[iCnt].code][len - j] = temp;
                 }
               }
             }
           } /*Generate code table*/
 
 
-          hf_buf = malloc(sizeof(uchar) * HF_BUF_SIZE);
+          //hf_buf = malloc(sizeof(uchar) * HF_BUF_SIZE);
 
           rewind(f_in);
 
           FILE *f_out = fopen(argv[3], "w");
 
           uint hf_buf_offset = 0;
+          uint wr_buf_offset = 0;
           uchar symbol = 0;
           uchar hf_code_len = 0;
           uchar *hf_code_ptr = NULL;
           int h = 0;
 
+          /*Saving header and code table*/
+          wr_buf[0] = 'h';
+          wr_buf[1] = 'f';
+          wr_buf[2] = 'x';
+          wr_buf[3] = 0xFF^'h';
+          wr_buf[4] = 0xFF^'f';
+          wr_buf[5] = 0xFF^'x';
+
+          wr_buf_offset = 6;
+
+          for (iCnt = 0; iCnt < 256; iCnt++)
+          {
+            hf_code_len = code_tbl[iCnt][0];
+            hf_code_ptr = &(code_tbl[iCnt][1]);
+
+            wr_buf[wr_buf_offset] = iCnt;
+            wr_buf_offset++;
+            wr_buf[wr_buf_offset] = hf_code_len;
+            wr_buf_offset++;
+            if (hf_code_len > 0)
+            {
+              memcpy(wr_buf+wr_buf_offset, hf_code_ptr, hf_code_len);
+              wr_buf_offset += hf_code_len;
+            }
+          }
+          fwrite(wr_buf, 1, wr_buf_offset, f_out);
+
           do
           {
             bytes_read = fread(rd_buf, 1, RD_BUF_SIZE, f_in);
 
-            for (i = 0; i < bytes_read; i++)
+            for (iCnt = 0; iCnt < bytes_read; iCnt++)
             {
-              symbol = rd_buf[i];
+              symbol = rd_buf[iCnt];
               hf_code_len = code_tbl[symbol][0];
               hf_code_ptr = &(code_tbl[symbol][1]);
+              //printf("Code %.2X length %d ", symbol, hf_code_len);
 
               if (hf_buf_offset + hf_code_len < HF_BUF_SIZE)
               {
+                //printf("Append byte %.2X\n", symbol);
                 memcpy(hf_buf + hf_buf_offset, hf_code_ptr, hf_code_len);
                 hf_buf_offset += hf_code_len;
               }
@@ -290,6 +320,7 @@ int main(int argc, char *argv[])
                 /*Move hf_buf contents to wr_buf*/
                 /*No need to check buffer overflow cause wr_buf size*/
                 /*depends on hf_buf size*/
+                //printf("Flush %d bytes \n", hf_buf_offset);
                 for (h = 0; h < hf_buf_offset; h += 8)
                 {
                   wr_buf[h/8] = (hf_buf[h    ] * 0x01 + 
@@ -301,15 +332,20 @@ int main(int argc, char *argv[])
                                  hf_buf[h + 6] * 0x40 +
                                  hf_buf[h + 7] * 0x80) & 0xFF; 
                 }
+                //printf("Bytes left %d\n", hf_buf_offset);
                 /*if 7 (or less) bytes left in hf_buf moving them to the zero index*/
                 /*and updating hf_buf_offset accordingly*/
                 if (h > hf_buf_offset)
                 {
                   memmove(hf_buf, hf_buf+(h-8), hf_buf_offset-(h-8));
-                  hf_buf_offset -= h;
+                  hf_buf_offset -= (h-8);
                 }
+                else 
+                  hf_buf_offset = 0;
 
                 fwrite(wr_buf, 1, h/8, f_out);
+
+                //printf("Bytes left %d\n", hf_buf_offset);
               }
             }
           }
@@ -349,6 +385,7 @@ int main(int argc, char *argv[])
   	{
   		/*Going to extract*/
   		printf ("Extracting, please wait...\n");
+    
   	}
   	else
   	{
